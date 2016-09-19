f8c39864faef81ac0b0e32f9d35a20d9975a9d58
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-09-19 19:07:50 +0700

    my back hurts

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require('express'),
       app = express(),
-      mongoose = require('mongoose');
+      mongoose = require('mongoose'),
+      body-parser = require('body-parser'),
+      morgan = require('morgan');
 
 require('dotenv').load();
